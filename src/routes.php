60e04039246527c6ea4b215b072392af76ee294b
Derek Myers <arcticpro@gmail.com>
2014-03-31 23:46:51 -0500

    Changed routes to use shorthand for controller routing

--- src/routes.php
+++ src/routes.php
@@ -4,9 +4,9 @@ if (Pages::homePage()) {
 	Route::get(Pages::routePath().Pages::homePage(), function() {
 		return Redirect::to('/');
 	});
-	Route::get('/', array('uses' => 'PagesController@getIndex'));
+	Route::get('/', 'PagesController@getIndex');
 }
 
 $pages = Pages::listPages();
 $regex = implode('|', $pages);
-Route::get(Pages::routePath().'{path?}', array('uses' => 'PagesController@getIndex'))->where('path', $regex);
+Route::get(Pages::routePath().'{path?}', 'PagesController@getIndex')->where('path', $regex);
