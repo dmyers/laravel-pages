a52ebcaf7e3eb0df0efb679ede75130ab467f40e
Derek Myers <arcticpro@gmail.com>
2019-10-17 23:12:24 -0500

    Changed code syntax in service provider

--- src/Dmyers/Pages/PagesServiceProvider.php
+++ src/Dmyers/Pages/PagesServiceProvider.php
@@ -3,38 +3,37 @@
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
 
-class PagesServiceProvider extends ServiceProvider {
-	
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		include __DIR__.'/../../helpers.php';
-		
-		if (\Config::get('pages.routes', true)) {
-			$this->app->router->group(['namespace' => 'Dmyers\Pages'], function($router) {
-				include __DIR__.'/../../routes.php';
-			});
-		}
-		
-		$this->publishes([
-			__DIR__.'/../../config/pages.php' => config_path('pages.php'),
-		]);
-	}
-	
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->bind('pages', function($app) {
-			return new Pages;
-		});
-	}
-	
+class PagesServiceProvider extends ServiceProvider
+{    
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/../../helpers.php';
+        
+        if (\Config::get('pages.routes', true)) {
+            $this->app->router->group(['namespace' => 'Dmyers\Pages'], function($router) {
+                include __DIR__.'/../../routes.php';
+            });
+        }
+        
+        $this->publishes([
+            __DIR__.'/../../config/pages.php' => config_path('pages.php'),
+        ]);
+    }
+    
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('pages', function($app) {
+            return new Pages;
+        });
+    }    
 }
